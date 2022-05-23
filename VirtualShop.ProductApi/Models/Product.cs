267755bd01e2db89e4acc05aa7efde08fb8496bf
eduardo.chiamulera <48774949+eduardochiamulera@users.noreply.github.com>
2022-05-23 10:33:28 -0300

    created context db

---
+++ VirtualShop.ProductApi/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace VirtualShop.ProductApi.Models;
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public long Stock { get; set; }
+    public string? ImageURL { get; set; }
+
+    //Navigation Properties
+    public Category? Category { get; set; }
+    public int CategoryId { get; set; }
+}
