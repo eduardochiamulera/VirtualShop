3363992f67f920adc8f192791604badc8d227d05
Eduardo Fraga <eduardofraga1994@gmail.com>
2022-05-23 14:52:09 -0300

    seed table products

---
+++ VirtualShop.ProductApi/Migrations/20220523174658_SeedProducts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtualShop.ProductApi.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Inset into Products(Name, Price, Description, Stock, ImageUrl, CategoryId" + 
+                "Values('Caderno', 7.55M, 'Caderno',10,'caderno1.jpg',1");
+
+            migrationBuilder.Sql("Inset into Products(Name, Price, Description, Stock, ImageUrl, CategoryId" +
+                "Values('Lápis', 3.45M, 'Lápis Preto',20,'lapis1.jpg',1");
+
+            migrationBuilder.Sql("Inset into Products(Name, Price, Description, Stock, ImageUrl, CategoryId" +
+                "Values('Clips', 5.33M, 'Clips para papel',50,'clips1.jpg',2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Products");
+        }
+    }
+}
