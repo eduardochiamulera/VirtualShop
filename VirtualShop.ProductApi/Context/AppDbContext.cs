9909f45b0c1100465e7608c30099ac014ff29708
Eduardo Fraga <eduardofraga1994@gmail.com>
2022-05-23 14:51:57 -0300

    useing fluent API to modeling

--- VirtualShop.ProductApi/Context/AppDbContext.cs
+++ VirtualShop.ProductApi/Context/AppDbContext.cs
@@ -9,5 +9,34 @@ namespace VirtualShop.ProductApi.Context
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Category
+            modelBuilder.Entity<Category>().HasKey(c => c.Id);
+            modelBuilder.Entity<Category>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+            modelBuilder.Entity<Category>().HasMany(c => c.Products).WithOne(x => x.Category).IsRequired().OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                Id = 1,
+                Name = "Material Escolar"
+            },
+            new Category
+            {
+                Id = 2,
+                Name = "Acessórios"
+            }
+            );
+
+            //Product
+            modelBuilder.Entity<Product>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<Product>().Property(c => c.Description).HasMaxLength(255).IsRequired();
+            modelBuilder.Entity<Product>().Property(c => c.ImageURL).HasMaxLength(255).IsRequired();
+            modelBuilder.Entity<Product>().Property(c => c.Price).HasPrecision(12,2);
+
+        }
     }
 }
