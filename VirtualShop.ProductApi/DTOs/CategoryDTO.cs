91ce4376dd9b61a4d4f30b485da0ae87898801a2
Eduardo Fraga <eduardofraga1994@gmail.com>
2022-05-26 10:33:37 -0300

    create DTOs

---
+++ VirtualShop.ProductApi/DTOs/CategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VirtualShop.ProductApi.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+     
+        [Required(ErrorMessage ="The Name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+        public ICollection<ProductDTO>? Products { get; set; }
+    }
+}
