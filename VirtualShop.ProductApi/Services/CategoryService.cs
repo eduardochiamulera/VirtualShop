7779c28099b7c39d87adec058c218152d9a64bf7
Eduardo Fraga <eduardofraga1994@gmail.com>
2022-05-26 10:54:04 -0300

    created services

---
+++ VirtualShop.ProductApi/Services/CategoryService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using VirtualShop.ProductApi.DTOs;
+using VirtualShop.ProductApi.Models;
+using VirtualShop.ProductApi.Repositories;
+
+namespace VirtualShop.ProductApi.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            var categoriesEntities = await _categoryRepository.GetAll();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntities);
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategoriesProducts()
+        {
+            var categoriesEntities = await _categoryRepository.GetCategoriesProducts();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntities);
+        }
+
+        public async Task<CategoryDTO> GetCategoryById(int id)
+        {
+            var categoryEntity = await _categoryRepository.GetById(id);
+            return _mapper.Map<CategoryDTO>(categoryEntity);
+        }
+
+        public async Task AddCategory(CategoryDTO categoryDto)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDto);
+            await _categoryRepository.Create(categoryEntity);
+            categoryDto.Id = categoryEntity.Id;
+        }
+
+        public async Task UpdateCategory(CategoryDTO categoryDto)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDto);
+            await _categoryRepository.Update(categoryEntity);
+        }
+
+        public async Task RemoveCategory(int id)
+        {
+            await _categoryRepository.Delete(id);
+        }
+
+    }
+}
---
+++ VirtualShop.ProductApi/Services/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using VirtualShop.ProductApi.DTOs;
+using VirtualShop.ProductApi.Models;
+using VirtualShop.ProductApi.Repositories;
+
+namespace VirtualShop.ProductApi.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productsEntities = await _productRepository.GetAll();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntities);
+        }
+
+        public async Task<ProductDTO> GetProductById(int id)
+        {
+            var productEntity = await _productRepository.GetById(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task AddProduct(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.Create(productEntity);
+            productDTO.Id = productEntity.Id;
+        }
+
+        public async Task UpdateProduct(ProductDTO ProductDTO)
+        {
+            var productEntity = _mapper.Map<Product>(ProductDTO);
+            await _productRepository.Update(productEntity);
+        }
+
+        public async Task RemoveProduct(int id)
+        {
+            await _productRepository.Delete(id);
+        }
+
+    }
+}
